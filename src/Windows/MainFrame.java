7978560ed033dd1c3269a62bd03833e3009a7f19
swkang99 <kseongwoo99@gmail.com>
2021-12-14 03:50:36 +0900

    finish this project

--- src/Windows/MainFrame.java
+++ src/Windows/MainFrame.java
@@ -9,7 +9,7 @@ public class MainFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     public MainFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("GUI Test");
+        setTitle("Library Manager");
         setSize(800, 500);
         createMenu();
         setVisible(true);
